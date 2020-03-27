421a7cbfc9b3c307127dbd048df49359d1a109e9
matao <matao-0426@163.com>
2020-03-27 13:44:16 +0800

    init

---
+++ widget/src/main/java/com/smasher/iwidget/struct/FunctionException.java
@@ -0,0 +1,14 @@
+package com.smasher.iwidget.struct;
+
+/**
+ *
+ * @author Happiness
+ * @date 2017/5/16
+ */
+
+public class FunctionException extends Exception {
+
+    public FunctionException(String exception) {
+        super(exception);
+    }
+}
